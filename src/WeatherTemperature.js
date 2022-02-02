fdaa83b1b0843a02f09544b520a9bee28a05affb
Tiziana Mancini <tizianabmancini@gmail.com>
2022-02-02 18:21:43 -0500

    Added celsius and fahreheit buttons

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="units">
+          ºC |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            ºF
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            ºC
+          </a>
+          | ºF
+        </span>
+      </span>
+    );
+  }
+}
